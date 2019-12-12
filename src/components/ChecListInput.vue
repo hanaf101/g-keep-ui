c3aadae0b94ae80054a4f8b96ae901c5f5e07f23
Mohammed Hanaf <hanaf.n.m@gmail.com>
2019-12-12 09:48:50 +0530

    Added inputs for checklist

--- src/components/ChecListInput.vue
+++ src/components/ChecListInput.vue
@@ -1,6 +1,20 @@
 <template>
 <li class="list-item" :class="{done : done}">
+    <form id="chart_datas"  >
+      <div class="grid-container">
+        <div class="grid-x grid-padding-x input_wrp">
+          <div class="small-1 cell column">
+            <label>
+              <input type="text" placeholder="id">
+            </label>
+          </div>
+         
+        </div>
+        
+         <div class="addRowBtn" v-on:click.prevent="addRow" value=""><i class="fa fa-plus" aria-hidden="true"></i></div>
+      </div>  
 
+    </form>
 
 </li>    
 
